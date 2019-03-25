95e9f6c362ec11e9110dd27f9a24b18358d7d1c7
qoorus <q7254task@gmail.com>
2019-03-25 16:16:37 +0900

    IBOutletにて動的に献立が生成される仕組みを実装

--- MeshiPeroApp/ViewController.swift
+++ MeshiPeroApp/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var menu: UITextField!
+    @IBOutlet var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text = "カレーライス"
     }
 
 
