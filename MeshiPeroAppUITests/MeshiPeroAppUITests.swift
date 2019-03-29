81c5a00510023b1b31f8ced12da38fe8bb69e407
qoorus <q7254task@gmail.com>
2019-03-29 15:54:40 +0900

    label.text = 「カレーライス」という振る舞いをテストしようとしたら失敗

--- MeshiPeroAppUITests/MeshiPeroAppUITests.swift
+++ MeshiPeroAppUITests/MeshiPeroAppUITests.swift
@@ -29,7 +29,8 @@ class MeshiPeroAppUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
+        let label = UILabel()
+        XCTAssertEqual(label.text, "カレーライス")
     }
 
 }
