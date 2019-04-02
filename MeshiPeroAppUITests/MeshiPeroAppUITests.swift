95683f7d827aa0c0acda6b641b54fc5ddec30b7e
qoorus <q7254task@gmail.com>
2019-04-02 14:02:20 +0900

    UIテストでやることをコメントに残し、AssertEqualを追加

--- MeshiPeroAppUITests/MeshiPeroAppUITests.swift
+++ MeshiPeroAppUITests/MeshiPeroAppUITests.swift
@@ -9,7 +9,8 @@
 import XCTest
 
 class MeshiPeroAppUITests: XCTestCase {
-
+    let app = XCUIApplication()
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -17,7 +18,7 @@ class MeshiPeroAppUITests: XCTestCase {
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -29,9 +30,12 @@ class MeshiPeroAppUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let app = XCUIApplication()
-        let label = app.staticTexts["label"]
         
+        // UIラベルを取得
+        let label = app.staticTexts["カレーライス"]
+        
+        // ラベルのテキストが「カレーライス」になっていれば成功
+        XCTAssertEqual(label.text, "カレーライス")
     }
 
 }
