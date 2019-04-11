698caa91560cc2ad4aec8974b4fa517d6b9ff5e4
qoorus <q7254task@gmail.com>
2019-04-11 15:38:54 +0900

    ViewController2.swiftを追加

--- MeshiPeroApp/ViewController2.swift
+++ MeshiPeroApp/ViewController2.swift
@@ -1,24 +1,23 @@
 //
-//  ViewController.swift
+//  ViewController2.swift
 //  MeshiPeroApp
 //
-//  Created by EarthCampus, Inc. on 2019/03/11.
+//  Created by EarthCampus, Inc. on 2019/04/11.
 //  Copyright © 2019 EarthCampus, Inc. All rights reserved.
 //
 
 import UIKit
 
-class ViewController1: UIViewController {
-
-    @IBOutlet weak var label_1: UILabel!
+class ViewController2: UIViewController {
+    
+    @IBOutlet weak var label_2: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        label_1.text = "カレーライス"
+        label_2.text = "チャーハン"
         
     }
-
-
+    
+    
 }
-
