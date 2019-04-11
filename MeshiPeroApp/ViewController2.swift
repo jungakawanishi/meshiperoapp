c2a4fa41b6d02b02123f5c7a0d439733a42e0d87
qoorus <q7254task@gmail.com>
2019-04-11 17:25:49 +0900

    ViewController2.swiftをView Controller Sceneに接続

---
+++ MeshiPeroApp/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2ViewController.swift
+//  MeshiPeroApp
+//
+//  Created by qoorus on 2019/04/11.
+//  Copyright © 2019 EarthCampus, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
