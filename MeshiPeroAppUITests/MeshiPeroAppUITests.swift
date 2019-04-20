00497b7642b77c7dc00e52f0b7ffb9a57b7ba221
qoorus <q7254task@gmail.com>
2019-04-20 11:27:03 +0900

    変数指定を削除

--- MeshiPeroAppUITests/MeshiPeroAppUITests.swift
+++ MeshiPeroAppUITests/MeshiPeroAppUITests.swift
@@ -16,8 +16,7 @@ class MeshiPeroAppUITests: XCTestCase {
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+       // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
@@ -33,9 +32,8 @@ class MeshiPeroAppUITests: XCTestCase {
         
         let window = app.windows.element(boundBy: 0)
         
-        var curryRiceLabelElement = app.staticTexts["CurryRiceViewController_Label"]
-        XCTAssertEqual(curryRiceLabelElement.label, "カレーライス")
-        XCTAssert(window.frame.contains(curryRiceLabelElement.frame))
+        XCTAssertEqual(app.staticTexts["CurryRiceViewController_Label"].label, "カレーライス")
+        XCTAssert(window.frame.contains(app.staticTexts["CurryRiceViewController_Label"].frame))
         
         app.swipeRight()
         XCTAssertEqual(app.staticTexts["FriedRiceViewController_Label"].label, "チャーハン")
