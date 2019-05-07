6359df415a1bcc1dad7bcd98d24f25f484c87993
qoorus <q7254task@gmail.com>
2019-05-07 16:27:51 +0900

    ボタンを押すと入力した献立が配列に追加されて、入力欄がリセットされるコードを実装

--- MeshiPeroApp/ViewController.swift
+++ MeshiPeroApp/ViewController.swift
@@ -10,20 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     @IBOutlet weak var menuLabel: UILabel!
+    
+    @IBOutlet weak var menuTextField: UITextField!
     
     @IBAction func rightSwiped(_ sender: UISwipeGestureRecognizer) {
     }
     
+    var textFieldString = ""
+    
+    var menu = ["カレーライス", "チャーハン", "ギョーザ", "ラーメン", "八宝菜", "唐揚げ"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var menu = ["カレーライス", "チャーハン", "ギョーザ", "ラーメン", "八宝菜", "唐揚げ"]
         let i = Int.random(in: 0..<menu.count)
         menuLabel.text = menu[i]
         
     }
 
+    @IBAction func pushButton(_ sender: UIButton) {
+
+        textFieldString = menuTextField.text!
+        
+        menu += [textFieldString]
+        
+        menuTextField.text = ""
+    }
 
 }
 
