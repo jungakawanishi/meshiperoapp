af2ba6b2b61f971a62f1111a8b1d81feb98640a0
qoorus <q7254task@gmail.com>
2019-05-22 17:21:34 +0900

    UITestsのファイルをViewごとに分割

--- MeshiPeroAppUITests/MeshiPeroAppAddMenuUITests.swift
+++ MeshiPeroAppUITests/MeshiPeroAppAddMenuUITests.swift
@@ -1,14 +1,15 @@
 //
-//  MeshiPeroAppUITests.swift
+//  MeshiPeroAppAddMenuUITests.swift
 //  MeshiPeroAppUITests
 //
-//  Created by EarthCampus, Inc. on 2019/03/11.
+//  Created by qoorus on 2019/05/22.
 //  Copyright © 2019 EarthCampus, Inc. All rights reserved.
 //
 
 import XCTest
 
-class MeshiPeroAppUITests: XCTestCase {
+class MeshiPeroAppAddMenuUITests: XCTestCase {
+
     let app = XCUIApplication()
     
     override func setUp() {
@@ -16,7 +17,8 @@ class MeshiPeroAppUITests: XCTestCase {
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-       // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
@@ -26,42 +28,6 @@ class MeshiPeroAppUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testSwipeToOpenAnotherView() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
-        let window = app.windows.element(boundBy: 0)
-        let menuLabelElement = app.staticTexts["PeroViewController_MenuLabel"]
-        
-        XCTAssert(window.frame.contains(menuLabelElement.frame), "初期表示でのUIWindow枠内にラベルが表示されていない")
-        
-        app.swipeRight()
-        XCTAssert(window.frame.contains(menuLabelElement.frame), "一度スワイプした後のUIWindow枠内にラベルが表示されていない")
-        
-        UserDefaults.standard.removeObject(forKey: "Menu")
-    }
-    
-    func testTabBarToSwitchView() {
-        
-        let window = app.windows.element(boundBy: 0)
-        let tabBarsQuery = app.tabBars
-        
-        tabBarsQuery.buttons["addMenu"].tap()
-        
-        let menuTextFieldElement = app.textFields["addMenuViewController_TextFieldToAddMenu"]
-        let buttonElement = app.buttons["addMenuViewController_Button"]
-        
-        XCTAssert(window.frame.contains(menuTextFieldElement.frame), "addMenuに画面を切り替えた時に、textFieldToAddMenuがUIWindow枠内に表示されていない")
-        XCTAssert(window.frame.contains(buttonElement.frame), "addMenuに画面を切り替えた時に、追加ボタンがUIWindow枠内に表示されていない")
-        
-        tabBarsQuery.buttons["pero"].tap()
-        
-        let menuLabelElement = app.staticTexts["PeroViewController_MenuLabel"]
-        
-        XCTAssert(window.frame.contains(menuLabelElement.frame), "peroに画面を切り替えた時に、menuLabelがUIWindow枠内に表示されていない")
-        
-    }
-    
     func testCanClearTextField() {
         
         let tabBarsQuery = app.tabBars
@@ -113,5 +79,4 @@ class MeshiPeroAppUITests: XCTestCase {
         XCTAssert(hogeCanBeIndicated, "新しい献立を追加して100回スワイプしても、その献立が表示されない")
         
     }
-    
 }
