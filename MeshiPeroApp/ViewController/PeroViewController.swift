5b2f9c4c12044b6dfeda83d26ebdb3ed49d63866
qoorus <q7254task@gmail.com>
2019-07-01 17:00:57 +0900

    PeroVCから画面遷移のコードを削除

--- MeshiPeroApp/ViewController/PeroViewController.swift
+++ MeshiPeroApp/ViewController/PeroViewController.swift
@@ -26,16 +26,7 @@ class PeroViewController: UIViewController {
         
     }
     
-    @IBAction func upSwiped(_ sender: UISwipeGestureRecognizer) {
-        performSegue(withIdentifier: "PeroSegue", sender: sender)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let next = segue.destination as? UITabBarController
-        let _ = next?.view
-        let nextPero = next?.viewControllers!.first{$0 is PeroViewController} as! PeroViewController
-        nextPero.presentMenus = nextMenus
-    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
